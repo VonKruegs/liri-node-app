bfa304e47764ebd981f2796a2026c4c237597c5f
VonKruegs <rob.krueger@gmail.com>
2019-07-30 18:16:06 -0400

    Got the bandsintown api working including in the event band info not available.

--- liri.js
+++ liri.js
@@ -1,27 +1,38 @@
-require("dotenv").config();
-var keys = require("./keys.js")
-var spotify = new Spotify(keys.spotify);
+//require("dotenv").config();
+//var keys = require("./keys.js")
+//var spotify = new Spotify(keys.spotify);
+var moment = require ("moment");
 
 var axios = require("axios");
+//var request = require("request");
 
+if (process.argv[2] == 'concert-this' ) {
+  console.log( 'concert this');
 
-// We then run the request with axios module on a URL with a JSON
-axios.get("https://www.npmjs.com/package/node-spotify-api").then(
-  function(response) {
-    // Then we print out the imdbRating
-    console.log(response);
-  }
-);
-var queryURL = "https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=[key]";
-    
-    console.log(queryURL); 
+  var artist = process.argv.slice(3).join(" ")
+  console.log(artist);
 
-    axios.get(queryURL).then(
-        function(bandResponse){
-            console.log("Venue: " + bandResponse.data[0].venue.name);
-            console.log("City: " + bandResponse.data[0].venue.city);
-            console.log(moment(bandResponse.data[0].datetime).format("MM/DD/YYYY"));
-        }
-    );
-//add movie spotify and the other random text functions
+  var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
+  axios.get(queryURL).then(function(response) {
+    // If the axios was successful...
+    // Then log the body from the site! 
+  
+    if (response.data.length === 0) {
+      console.log(artist + " is not coming anytime soon!");
+    }
+      
+    else {var result  =  response.data[0]
+      console.log(result);
+
+      console.log("Venue name " + result.venue.name);
+      console.log("Venue location " + result.venue.city);
+      console.log("Date of Event " +  moment(result.datetime).format("MM/DD/YYYY"));
+
+     };
+
+    });
+
+  };
+
+//add movie spotify and the other random text function
